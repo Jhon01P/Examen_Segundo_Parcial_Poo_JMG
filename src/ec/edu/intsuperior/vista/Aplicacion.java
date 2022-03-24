6141ac4d6c1520181e6607494726169127efc6ed
JhonP <JhonP@MiPc>
2022-03-23 21:15:21 -0500

    EXAMEN SEGUNDO PARCIAL

---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+/**
+ *
+ * @author JhonP
+ */
+public class Aplicacion {
+     public static void main(String[] args){
+}
+}
